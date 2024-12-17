41648ec072d9941aafba516a5aa60a236116da6c
aryan-srivastava1625 <ap9140@srmist.edu.in>
2024-12-17 20:43:08 +0530

    making employee dashboard

---
+++ src/components/other/TaskNumber.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TaskNumber = () => {
+  return (
+    <div className='flex mt-10 justify-between gap-5  screen'>
+        <div className='h-40 w[45%] bg-red-400'></div>
+      
+    </div>
+  )
+}
+
+export default TaskNumber
