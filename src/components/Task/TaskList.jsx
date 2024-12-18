3e6a600e95be1635971c2d5f09b7b8364018b636
aryan-srivastava1625 <ap9140@srmist.edu.in>
2024-12-18 12:30:25 +0530

    done with the dashboard

--- src/components/Task/TaskList.jsx
+++ src/components/Task/TaskList.jsx
@@ -14,8 +14,78 @@ const TaskList = () => {
 
         </p>
       </div>
+
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-pink-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>
       
-          
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-violet-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>
+
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-pink-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>
+
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-violet-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>
+
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-pink-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>
+
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-violet-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>    
     
     </div>
   )
