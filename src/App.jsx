86657d1ee3a6501b6f6df00afaa93b1e11c9cfc3
aryan-srivastava1625 <ap9140@srmist.edu.in>
2024-12-18 20:09:16 +0530

    setting the admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utility/localStorage'
+// import { getLocalStorage, setLocalStorage } from './utility/localStorage'
 
 const App = () => {
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage()
-  }, )
   
+  const [user, setUser] = useState(null)
+
+  const handleLogin=(email,password)=>{
+    if(email == 'admin@me.com' && password == '123'){
+      console.log("this is admin")
+    }else if(email == 'user@me.com' && password == '123'){
+      console.log("This is user")
+    }
+    else{
+      alert("Invalid Credentials")
+    }
+
+  }
+
+  // useEffect(() => {
+  //   // setLocalStorage()
+  //   getLocalStorage()
+  // }, )
+  
+  handleLogin('admin@me.com',123)
 
 
   return (
     <>
-    <Login/>
+    {!user ? <Login handleLogin={handleLogin}/ >:''}
     {/* <EmployeeDashboard/> */}
     {/* <AdminDashboard/> */}
     </>
