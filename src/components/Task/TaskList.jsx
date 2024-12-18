de168c8ef9418d2340782bf23adcc2c6e5da0ec2
aryan-srivastava1625 <ap9140@srmist.edu.in>
2024-12-18 12:14:31 +0530

    Making the tasklist

---
+++ src/components/Task/TaskList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center flex-nowrap justify-start gap-5 w-full  py-5  mt-10'>
+      <div className='flex-shrink-0 h-full p-5 w-[300px] bg-violet-950 rounded-xl'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className=' text-base rounded'>10 December 2024</h4>
+        </div>
+        <h2 className='mt-5 text-2xl font-semibold '>Prepare for the interviews</h2>
+        <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque ducimus eaque molestias rerum, illum dolor quisquam corporis earum dignissimos, facere nostrum cumque harum blanditiis! Maiores odit non reiciendis possimus quisquam?
+
+        </p>
+      </div>
+      
+          
+    
+    </div>
+  )
+}
+
+export default TaskList
